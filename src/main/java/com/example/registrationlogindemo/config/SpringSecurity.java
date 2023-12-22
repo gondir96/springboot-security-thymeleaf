2cd798b44d12c25f223c2aca498171c8b0fb4d58
Péter Kovács <kpeter.office@gmail.com>
2023-12-22 12:14:36 +0100

    redirecting after login fixed

--- src/main/java/com/example/registrationlogindemo/config/SpringSecurity.java
+++ src/main/java/com/example/registrationlogindemo/config/SpringSecurity.java
@@ -31,6 +31,7 @@ public class SpringSecurity {
                         authorize.requestMatchers("/register/**").permitAll()
                                 .requestMatchers("/index").permitAll()
                                 .requestMatchers("/users").hasRole("ADMIN")
+                                .anyRequest().permitAll()
                 ).formLogin(
                         form -> form
                                 .loginPage("/login")
